5f875f3c1fb050504728205d9110ea26a852d0f6
Mohamed R. Khan <khanantisora@hotmail.com>
2016-02-10 21:41:14 -0500

    Basket completed!
    The basket now catches (Destroys) the apples when they collide.

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -3,8 +3,19 @@ using System.Collections;
 
 public class ApplePicker : MonoBehaviour {
 
+	public GameObject basketPrefab;
+	public int numBaskets = 3;
+	public float basketBottomY = -14f;
+	public float basketSPacingY = 2f;
+
 	// Use this for initialization
 	void Start () {
+		for (int i=0; i<numBaskets; i++) {
+			GameObject tBasketGo = Instantiate(basketPrefab) as GameObject;
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBottomY + (basketSPacingY * i );
+			tBasketGo.transform.position = pos; 
+		}
 	
 	}
 	
